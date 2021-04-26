d984b371718e8aa4a214279870e6c740aef38da4
TuckerLandis <landistuckerc@gmail.com>
2021-04-26 18:28:12 -0500

    proof of concept on func addToCollection

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,1 +1,17 @@
-console.log('***** Music Collection *****')
+console.log('***** Music Collection *****');
+
+let collection = [];
+
+function addToCollection (title, artist, yearPublished){
+  console.log('in addToCollection ' + title, artist, yearPublished);
+  let x = title;
+  let y = artist;
+  let z = yearPublished;
+collection.push({title: x, artist: y, yearPublished: z});
+}
+
+
+addToCollection('Selected Public Works Vol. 5', 'Lightbath', 2020);
+addToCollection('Gunfighter Ballads and Trail Songs', 'Marty Robbins', 1959);
+
+console.log(collection);
