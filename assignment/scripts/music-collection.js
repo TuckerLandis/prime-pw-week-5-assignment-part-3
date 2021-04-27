0c137a51db98e632f8affdd5504b9c4fe102bb16
TuckerLandis <landistuckerc@gmail.com>
2021-04-26 21:05:15 -0500

    search is fully functional if i understand the question correctly

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -11,7 +11,7 @@ collection.push({title: x, artist: y, yearPublished: z});
 return collection[collection.length - 1];
 }
 
-
+console.log(addToCollection('Selected Public Works Vol. 6', 'Lightbath', 2020));
 console.log(addToCollection('Selected Public Works Vol. 5', 'Lightbath', 2020));
 console.log(addToCollection('Gunfighter Ballads and Trail Songs', 'Marty Robbins', 1959));
 console.log(addToCollection('Selected Public Works Vol. 4', 'Lightbath', 2019));
@@ -28,7 +28,7 @@ function showCollection(array) {
     console.log( item.title + ' by ' + item.artist + ', published in ' + item.yearPublished );
   }
   return collection;
-  // end looop
+  // end loop
 } // end showCollection
 
 showCollection(collection);
@@ -71,3 +71,26 @@ function findByArtist(artist){
 console.log(findByArtist('Splitta'));
 console.log(findByArtist('Lightbath'));
 console.log(findByArtist('Harrison BDP'));
+
+
+
+function search( artist, year ){
+  console.log('in search', artist, year);
+let results = [];
+let sA = artist;
+let sY = year;
+for (let i of collection) {
+  if (sA === i.artist && sY ===i.yearPublished) {
+    results.push(i);
+  } // end if
+  else if (sA == ' ' || sY == ' ') {
+    return collection;
+  }
+} // end loop
+return results;
+} // end function
+
+console.log(search('Splitta', 2020)); // 1 matching object
+console.log(search('Lightbath', 2020)); // 2 matching objects
+console.log(search( ' ', 2020)); // empty search value
+console.log(search('Cat Soup', 2020)); // not found 
