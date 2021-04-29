dfc6d883f5e2c7864558f13c49e7b38fc7202b9a
TuckerLandis <landistuckerc@gmail.com>
2021-04-28 20:17:55 -0500

    removed redundant variable declarations from addToCollection

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -4,14 +4,11 @@ let collection = [];
 
 function addToCollection (title, artist, yearPublished){
   console.log('in addToCollection ' + title, artist, yearPublished);
-  let x = title;
-  let y = artist;
-  let z = yearPublished;
-collection.push({title: x, artist: y, yearPublished: z});
+collection.push({title: title, artist: artist, yearPublished: yearPublished});
 return collection[collection.length - 1];
 }
 
-console.log(addToCollection('Selected Public Works Vol. 6', 'Lightbath', 2020));
+console.log(addToCollection('Selected Public Works Vol. 6', 'Lightbath', 2020, 'song1', '4:00'));
 console.log(addToCollection('Selected Public Works Vol. 5', 'Lightbath', 2020));
 console.log(addToCollection('Gunfighter Ballads and Trail Songs', 'Marty Robbins', 1959));
 console.log(addToCollection('Selected Public Works Vol. 4', 'Lightbath', 2019));
@@ -19,7 +16,6 @@ console.log(addToCollection('Toy Beats', 'Splitta', 2020));
 console.log(addToCollection('Nice Dog', 'Jon Corbett', 2019));
 console.log(addToCollection('Dust Bowl Ballads', 'Woody Guthrie', 1940));
 console.log(addToCollection('déchets inutiles', 'Watson', 2020));
-
 console.log(collection);
 
 function showCollection(array) {
@@ -72,8 +68,6 @@ console.log(findByArtist('Splitta'));
 console.log(findByArtist('Lightbath'));
 console.log(findByArtist('Harrison BDP'));
 
-
-
 function search( artist, year ){
   console.log('in search', artist, year);
 let results = [];
@@ -93,4 +87,4 @@ return results;
 console.log(search('Splitta', 2020)); // 1 matching object
 console.log(search('Lightbath', 2020)); // 2 matching objects
 console.log(search( ' ', 2020)); // empty search value
-console.log(search('Cat Soup', 2020)); // not found 
+console.log(search('Cat Soup', 2020)); // not found
